d99ffe19c7a31a0795417b421f81b6e71d15a7a7
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-03-27 14:57:22 -0300

    created remaining endpoints

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const uniqid = require("uniqid");
 const fs = require("fs");
 const path = require("path");
 const db = require("./db/db.json");
@@ -6,9 +7,11 @@ const db = require("./db/db.json");
 const app = express();
 const port = 5050;
 
+app.use(express.json());
+
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   const newPath = path.join(__dirname, "/public/index.html");
   res.sendFile(newPath);
 });
@@ -17,8 +20,38 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", async (req, res) => {
+app.get("/api/notes", (req, res) => {
+  console.log("GETTING NOTES");
   res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log("POSTING NOTE");
+  let dataArray = [];
+  if (db && db.length > 0) {
+    dataArray = [...db];
+  }
+
+  const note = { ...req.body, id: uniqid() };
+
+  dataArray.push(note);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(dataArray), (err) => {
+    if (err) throw err;
+  });
+
+  res.json(dataArray);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.log("DELETING NOTE");
+  if (req.params.id) {
+    let newArray = db.filter((note) => note.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(newArray), (err) => {
+      if (err) throw err;
+    });
+    res.json(newArray);
+  }
 });
 
 app.listen(port);
